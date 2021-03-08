59609e55350ab1c7dae2d91d075b44779eb1958f
Selemmerili <77363152+Selemmerili@users.noreply.github.com>
2021-03-08 23:36:23 +0100

    Mise à jour de l'api

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, jsonify,request
 from main import stations, next_transports,next_line_station_direction
-from station_file_3 import result3
 from station_file_1 import result1
 from station_file_2 import result2
+from station_file_3 import result3
 from flask_cors import CORS
 import sqlite3
 from main import main, update_db, load_csv, create_schema, insert_csv_row
@@ -16,19 +16,21 @@ app = Flask(__name__)
 main()
 @app.route('/')
 def home():
-    return render_template('index.html')
+    stations()
+    return render_template('home.html',title='Welcome')
 
 @app.route('/stations')
 def entry_point():
+    # jsonify(stations())
     # return render_template('./app.html')
-    # return jsonify(stations())
-    return render_template('Stations.html',
+    return render_template('Stations.html',title='Stations',
     blogs=result1)
 
 @app.route('/next/<station>')
 def demande(station):
-    return jsonify(next_transports(station))
-    return render_template('station_next.html',
+    
+    jsonify(next_transports(station))
+    return render_template('next.html',title='Next',
     blogs1=result2)
 
 # @app.route('/next/<line>/<station>/<direction>')
@@ -41,9 +43,21 @@ def next_infos():
     args1 = request.args['Ligne']
     args2 = request.args['Station']
     args3 = request.args['Direction']
+    (next_line_station_direction(args1,args2,args3))
+    return render_template('next_line.html',title='Your Trip',blogs2=result3)
 
-    return jsonify(next_line_station_direction(args1,args2,args3))
 
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port="5000", debug=True)
+# @app.route('/next', methods=['GET'])
+# def next_infos():
+
+#     args1 = request.args['Ligne']
+#     args2 = request.args['Station']
+#     args3 = request.args['Direction']
+#     jsonify(next_line_station_direction(args1,args2,args3))
+#     return render_template('next_line.html',
+#                         title='Your Trip',
+#                         blogs1=result1)
+
